bea55f48b299206b304912fc5e9ebbe50bc3762e
NotJofe <jofe3008@gmail.com>
2025-07-24 22:59:07 -0500

    add: modify & delete user

--- src/app/info-user/info-user.ts
+++ src/app/info-user/info-user.ts
@@ -2,20 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-info-user',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './info-user.html',
   styleUrl: './info-user.css',
 })
 export class InfoUserComponent implements OnInit {
-  nombre: string = '';
-  apellido: string = '';
-  email: string = '';
-  fechaNacimiento: string = '';
-  colegio: string = '';
-  rol: string = '';
+  nombre = '';
+  apellido = '';
+  email = '';
+  fechaNacimiento = '';
+  colegio = '';
+  rol = '';
+
+  // Variables de edición
+  modoEdicion = false;
+  datosOriginales: any = {};
+  passwordUsuario = ''; // Puede ser "" o fijo si el backend lo requiere
 
   constructor(private http: HttpClient) {}
 
@@ -24,39 +30,125 @@ export class InfoUserComponent implements OnInit {
     const userId = localStorage.getItem('userId');
 
     if (token && userId) {
-      const headers = new HttpHeaders({
-        Authorization: `Bearer ${token}`,
-      });
+      const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
 
-      this.http
-        .get<any>(`http://localhost:8080/api/v1/user/${userId}`, { headers })
-        .subscribe({
-          next: (user) => {
-            this.nombre = user.name;
-            this.apellido = user.lastName;
-            this.email = user.email;
-            this.fechaNacimiento = user.birthdate;
-            this.colegio = user.school;
-            this.rol = this.formatearRol(user.role.name);
-          },
-          error: (err) => {
-            console.error('Error al obtener usuario:', err);
-          },
-        });
+      this.http.get<any>(`http://localhost:8080/api/v1/user/${userId}`, { headers }).subscribe({
+        next: (user) => {
+          this.nombre = user.name;
+          this.apellido = user.lastName;
+          this.email = user.email;
+          this.fechaNacimiento = user.birthdate;
+          this.colegio = user.school;
+          this.rol = this.formatearRol(user.role.name);
+          this.passwordUsuario = user.password;
+
+          this.datosOriginales = { ...user };
+        },
+        error: (err) => console.error('Error al obtener usuario:', err),
+      });
     }
   }
+
   formatearRol(rolBackend: string): string {
     switch (rolBackend) {
-      case 'ROLE_ALUMNO':
-        return 'Alumno';
-      case 'ROLE_DOCENTE':
-        return 'Docente';
-      case 'ROLE_DIRECTOR':
-        return 'Director';
-      case 'ROLE_ADMIN':
-        return 'Administrador';
-      default:
-        return 'Usuario';
+      case 'ROLE_ALUMNO': return 'Alumno';
+      case 'ROLE_DOCENTE': return 'Docente';
+      case 'ROLE_DIRECTOR': return 'Director';
+      case 'ROLE_ADMIN': return 'Administrador';
+      default: return 'Usuario';
     }
   }
+
+  toggleEdicion() {
+    if (this.modoEdicion) {
+      const modal = new bootstrap.Modal(document.getElementById('modalConfirmarGuardado')!);
+      modal.show();
+    } else {
+      this.modoEdicion = true;
+    }
+  }
+
+  cancelarEdicion() {
+    this.nombre = this.datosOriginales.name;
+    this.apellido = this.datosOriginales.lastName;
+    this.fechaNacimiento = this.datosOriginales.birthdate;
+    this.colegio = this.datosOriginales.school;
+    this.modoEdicion = false;
+  }
+
+  confirmarGuardado() {
+    const token = localStorage.getItem('accessToken');
+    const userId = localStorage.getItem('userId');
+
+    if (!token || !userId) return;
+
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+
+    const payload = {
+      name: this.nombre,
+      lastName: this.apellido,
+      birthdate: this.fechaNacimiento,
+      email: this.email,
+      //password: this.passwordUsuario,
+      school: this.colegio,
+    };
+    console.log('🔼 JSON que se envía al backend:', payload);
+
+    this.http.put<any>(`http://localhost:8080/api/v1/user/${userId}`, payload, { headers }).subscribe({
+      next: (res) => {
+        this.modoEdicion = false;
+        this.datosOriginales = { ...res };
+        this.http.get<any>(`http://localhost:8080/api/v1/user/${userId}`, { headers }).subscribe({
+        next: (user) => {
+          this.nombre = user.name;
+          this.apellido = user.lastName;
+          this.email = user.email;
+          this.fechaNacimiento = user.birthdate;
+          this.colegio = user.school;
+          this.rol = this.formatearRol(user.role.name);
+          this.passwordUsuario = user.password;
+
+          this.datosOriginales = { ...user };
+        },
+        error: (err) => console.error('Error al obtener usuario:', err),
+      });
+        console.log('✅ JSON de respuesta del backend:', res);
+        alert('✅ Información actualizada con éxito');
+      },
+      error: (err) => {
+        console.error('Error al actualizar:', err);
+        alert('❌ No se pudo actualizar la información');
+      },
+    });
+  }
+  
+
+  confirmarEliminacion() {
+  const userId = localStorage.getItem('userId');
+  const token = localStorage.getItem('accessToken');
+
+  if (!userId || !token) {
+    console.error('No se encontró el usuario o token.');
+    return;
+  }
+
+  const headers = new HttpHeaders({
+    Authorization: `Bearer ${token}`,
+  });
+
+  this.http
+    .delete(`http://localhost:8080/api/v1/user/${userId}`, { headers })
+    .subscribe({
+      next: () => {
+        console.log('Cuenta eliminada correctamente');
+        localStorage.clear(); // Limpia todos los datos
+        window.location.href = '/'; // Redirige al inicio
+      },
+      error: (err) => {
+        console.error('Error al eliminar la cuenta:', err);
+        alert('Ocurrió un error al eliminar tu cuenta.');
+      },
+    });
+}
+
 }
--- src/app/universidades-carrera/universidades-carrera.ts
+++ src/app/universidades-carrera/universidades-carrera.ts
@@ -4,17 +4,21 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { NgStyle, NgForOf, NgClass } from '@angular/common';
 
-
 @Component({
   selector: 'app-universidades-carrera',
   templateUrl: './universidades-carrera.html',
   styleUrl: './universidades-carrera.css',
   standalone: true,
-  imports: [NgForOf, NgStyle, NgClass]
+  imports: [NgForOf, NgStyle, NgClass],
 })
 export class UniversidadesCarreraComponent implements OnInit {
   nombreCarrera: string = '';
   universidades: any[] = [];
+  carrera: {
+    name: string;
+    description: string;
+    imageUrl: string;
+  } | null = null;
 
   constructor(
     private route: ActivatedRoute,
@@ -26,28 +30,45 @@ export class UniversidadesCarreraComponent implements OnInit {
     const carreraParam = this.route.snapshot.paramMap.get('career');
 
     if (!carreraParam) return;
-
-    this.nombreCarrera = carreraParam.replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase());
+    this.nombreCarrera = carreraParam
+      .replace(/-/g, ' ')
+      .replace(/\b\w/g, (l) => l.toUpperCase());
 
     const token = localStorage.getItem('accessToken');
     const headers = { Authorization: `Bearer ${token}` };
 
-    this.http.get<any[]>(`http://localhost:8080/api/v1/universitycareer/${carreraParam}`, { headers })
+    this.http
+      .get<any[]>(
+        `http://localhost:8080/api/v1/universitycareer/${carreraParam}`,
+        { headers }
+      )
       .subscribe({
-        next: (res) => this.universidades = res,
-        error: (err) => console.error('Error al obtener universidades:', err)
+        next: (res) => (this.universidades = res),
+        error: (err) => console.error('Error al obtener universidades:', err),
+      });
+      
+    this.http
+      .get<any>(`http://localhost:8080/api/v1/career/${carreraParam}`, {
+        headers,
+      })
+      .subscribe({
+        next: (res) => {
+          this.carrera = res;
+        },
+        error: (err) => {
+          console.error('Error al obtener detalles de la carrera:', err);
+          alert('No se pudo cargar la información de la carrera.');
+        },
       });
   }
   volverAResultados() {
-  this.router.navigate(['/resultados']);
-}
-hexToRGBA(hex: string, alpha: number): string {
-  const r = parseInt(hex.slice(1, 3), 16);
-  const g = parseInt(hex.slice(3, 5), 16);
-  const b = parseInt(hex.slice(5, 7), 16);
-  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
-}
-activo: number | null = null;
-
-
+    this.router.navigate(['/resultados']);
+  }
+  hexToRGBA(hex: string, alpha: number): string {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  }
+  activo: number | null = null;
 }
